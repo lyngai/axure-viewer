db4076cc1c08cd7604dc0be131bb5da87451b129
lamyin_ngai <wlxjust@gmail.com>
2018-08-05 11:09:34 +0800

    init project with element-ui

---
+++ src/layout/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer">
+        <p>Axure Viewer By LamYin Ngai</p>
+        <p>Themed By Element-ui</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      activeIndex: '1',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.footer {
+    font-size: 13px;
+    color: #909399;
+    border-top: solid 1px #e6e6e6;
+    padding: 15px;
+}
+</style>
