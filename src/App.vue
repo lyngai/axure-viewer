b08f6f3fd72b80a037661e92298bc7d8e3c030db
lamyin_ngai <wlxjust@gmail.com>
2018-08-05 11:09:34 +0800

    init project with element-ui

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-container id="app">
+    <el-header>
+      <ax-header/>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+    <el-footer style="height: auto">
+      <ax-footer/>
+    </el-footer>
+  </el-container>
+<!--   <div id="app">
+    <el-row type="flex" class="row-bg" justify="center">
+      <el-col :span="16">
+      </el-col>
+    </el-row>
+  </div> -->
+</template>
+
+<script>
+
+import Header from '@/layout/Header';
+import Footer from '@/layout/Footer';
+
+export default {
+  name: 'App',
+  components: {
+    'ax-header': Header,
+    'ax-footer': Footer,
+  },
+};
+</script>
+
+<style>
+body{
+  margin: 0;
+}
+#app {
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC",
+      "Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+.el-footer{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
