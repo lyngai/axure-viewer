73f6ca0c665e525a4a99e666d53bdad683308529
lamyin_ngai <wlxjust@gmail.com>
2018-08-07 15:35:39 +0800

    update: 更新解压方式及调用包路径

--- server/scripts/unzip.js
+++ server/scripts/unzip.js
@@ -1,24 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 // const unzip = require('unzip');
-const unzip = require('adm-zip');
+const unzip = require('./adm-zip');
 
 // const extractZip = (file, targetPath, onClose) => {
 // 	fs.createReadStream(file)
 // 	  .pipe(unzip.Extract({ path: targetPath }))
 // 	  .on('close', onClose);
 // };
-
+// return entry
 const extractZip = (file, targetPath, entry, onClose) => {
-	const zip = new unzip(file);
-	// const entries = zip.getEntries();
-	// // console.dir(entries);
-	// entries.forEach((entry) => {
-	// 	console.log(entry.entryName);
-	// });
-	console.log(file, targetPath, entry);
-	// zip.extractEntryTo(entry+'/', targetPath, false, false);
-	zip.extractAllTo(path.resolve(targetPath), true);
+	try{
+		const zip = new unzip(file);
+		const entries = zip.getEntries();
+		let entry_type = -1;
+		for(let e of entries) {
+			if(/\/index\.html$/.test(e.entryName)){
+				entry = e.entryName.split('/').shift();
+				entry_type = 1;
+				break;
+			} else if(e.entryName == 'index.html') {
+				entry_type = 0;
+				break;
+			}
+		}
+		if(entry_type !== -1) {
+			zip.extractAllTo(path.resolve(targetPath), true);
+			return entry_type == 1 ? entry : '';
+		} else {
+			return null;
+		}
+	} catch (e) {
+		console.error(e);
+		return null;
+	}
 };
 
 module.exports = {
